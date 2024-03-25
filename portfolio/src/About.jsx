d86f67a1e4e90e9be8a9430840d063ad889bc1c8
ZLucas2021 <89589591+zLucas2021@users.noreply.github.com>
2024-03-25 14:24:10 -0300

    create view about

---
+++ portfolio/src/About.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TopBar from './components/TopBar'
+import Footer from './components/Footer'
+function About() {
+
+  return (
+    <>
+    <TopBar/>
+    <Footer />
+    </>
+  )
+}
+
+export default About
