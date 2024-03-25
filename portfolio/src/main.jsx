514848c732b7c02daf6605d72178717c465dcd8b
ZLucas2021 <89589591+zLucas2021@users.noreply.github.com>
2024-03-25 14:25:33 -0300

    add routes

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import About from './About.jsx';
+const router = createHashRouter([
+  {
+    path:'/',
+    element: <App />
+  },
+  {
+    path:'/about',
+    element : <About/>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
